5c1530412b355c66ffb6e3f3610dad735acf7b37
Mike-Mp <mikemp@protonmail.com>
2022-04-13 23:17:32 +0300

    add gitignore in catr, finish test_parse_positive_int on headr

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,15 +1,13 @@
-use clap::{Arg, Command};
-use std::fs::File;
-use std::io::{self, BufRead, BufReader};
-use std::{any::Any, error::Error};
+use clap::{Command, Arg};
+use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
-    bytes: Option<usize>,
     lines: usize,
+    bytes: Option<usize>,
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -18,94 +16,71 @@ pub fn get_args() -> MyResult<Config> {
         .author("Mike-Mp")
         .about("Rust head")
         .arg(
-            Arg::new("files")
-            .value_name("FILES")
-            .allow_invalid_utf8(true)
-            .multiple_values(true)
-            .default_value("-")
-            .help("File name for cat to display it's contents")
+            Arg::new("lines")
+            .short('n')
+            .long("lines")
+            .default_value("10")
+            .conflicts_with("bytes")
+            .takes_value(true)
         )
         .arg(
             Arg::new("bytes")
             .short('c')
             .long("bytes")
-            .value_name("BYTES")
             .takes_value(true)
-            .conflicts_with("lines")
-            .help("print the first NUM bytes of each file; with the leading '-', print all but the last NUM bytes of each file")
         )
         .arg(
-            Arg::new("lines")
-            .short('n')
-            .long("lines")
-            .value_name("LINES")
-            .default_value("10")
+            Arg::new("files")
             .takes_value(true)
-            .help("print the first NUM lines instead of the first 10; with the leading '-', print all but the last NUM lines of each file")
+            .default_value("-")
+            .required(true)
         )
         .get_matches();
 
-    let bytes = matches
-        .value_of("bytes")
-        .map(parse_positive_int)
-        .transpose()
-        .map_err(|e| format!("illegal byte count -- {}", e))?;
-
-    let lines = matches
-        .value_of("lines")
-        .map(parse_positive_int)
-        .transpose()
-        .map_err(|e| format!("illegal line count -- {}", e))?;
-
-    Ok(Config {
-        files: matches.values_of_lossy("files").unwrap(),
-        bytes,
-        lines: lines.unwrap(),
+    return Ok(Config {
+        files: vec!["one".to_string(), "two".to_string()],
+        lines: 10,
+        bytes: std::option::Option::Some(1),
     })
 }
 
-fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
-    match filename {
-        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
-        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
-    }
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    return match val.parse() {
+        Ok(v) => {
+            if v > 0 {
+                Ok(v)
+            } else {
+                Err(val.into())
+            }
+        },
+        Err(_) => Err(val.into()),
+    };
+
+    // way from book
+    // match val.parse() {
+    //     Ok(n) if n > 0 => Ok(n),
+    //     _ => Err(From::from(val)),
+    // }
 }
 
-fn parse_positive_int(val: &str) -> MyResult<usize> {
-    match val.parse() {
-        Ok(n) if n > 0 => Ok(n),
-        _ => Err(From::from(val)),
-    }
+#[test]
+fn test_parse_positive_int() {
+    let res = parse_positive_int("3");
+
+    println!("{:?}", res);
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3);
+
+    let res = parse_positive_int("foo");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
+
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_string());
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    for filename in config.files {
-        match open(&filename) {
-            Err(err) => eprintln!("{}: {}", filename, err),
-            Ok(mut i) => {
-                print_contents(&mut i, config.lines, config.bytes);
-            }
-        }
-    }
+    println!("{:#?}", config);
     Ok(())
-}
-
-fn print_contents(file: &mut Box<dyn BufRead>, lines: usize, bytes: Option<usize>) {
-    if bytes.is_none() {
-        for (i, line) in file.lines().enumerate() {
-            if i != lines {
-                println!("{}", line.unwrap());
-            } else {
-                break;
-            }
-        }
-    } else {
-        let mut buffer = Vec::new();
-
-        file.read_to_end(&mut buffer);
-
-        for value in buffer {
-            println!("{}", value);
-        }
-    } 
 }
